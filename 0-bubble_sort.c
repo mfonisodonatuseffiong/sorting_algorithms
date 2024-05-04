920d4d80581513aa0dce90a978df15fab65a7589
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-05-04 01:38:24 +0000

    0x1B. C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,46 +1,39 @@
-
 #include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * bubble_sort - sorts array of intgers
- *
- * @array: Pointer to int array
- * @size: size of array
+ * bubble_sort - Sort by swapping largest items to end of array
+ * @array: Array to be sorted
+ * @size: Size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int swaped = -1;
+	size_t j;
+	int swapped;
+	size_t temp;
+	size_t tempsize;
 
-	if (size < 2 || !size || !array)
+	if (array == NULL)
 		return;
-
-	for (i = 0; i < size - 1; i++)
+	tempsize = size;
+	swapped = 1;
+	while (swapped)
 	{
-		for (j = 0; j < size - 1 - i; j++)
+		swapped = 0;
+		j = 0;
+		while (j < tempsize - 1)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
 				print_array(array, size);
-				swaped = 0;
+				swapped = 1;
 			}
+			j++;
 		}
-		if (swaped == -1)
-			break;
+		tempsize = j;
 	}
 }
-
-/**
- * swap - Swaps the values of two integers using pointers.
- * @x: Pointer to the first integer.
- * @y: Pointer to the second integer.
- */
-void swap(int *x, int *y)
-{
-	int tmp = *x;
-
-	*x = *y;
-	*y = tmp;
-}
-
